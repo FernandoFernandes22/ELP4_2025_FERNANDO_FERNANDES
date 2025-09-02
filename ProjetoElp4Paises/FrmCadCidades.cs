0d53a717a511c3e21416a73ddedae5daa0de9d46
FernandoFernandes22 <fernandoolivfernandes@gmail.com>
2025-09-01 22:11:34 -0300

    first commit

---
+++ ProjetoElp4Paises/FrmCadCidades.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetoElp4Paises
+{
+    public partial class FrmCadCidades : ProjetoElp4Paises.FrmCadastros
+    {
+        FrmConsEstados oFrmConsEstados;
+        Cidades aCidade;
+        Controller aCtrl;
+        public FrmCadCidades()
+        {
+            InitializeComponent();
+            oFrmConsEstados = new FrmConsEstados();
+        }
+
+        public  void setFrmConsEstados(object obj)
+        {
+            if(obj != null)
+            {
+                oFrmConsEstados = (FrmConsEstados) obj;  
+            }
+        }
+        public override void ConhecaObj(object obj, object ctrl)
+        {
+            if (obj != null)
+                aCidade = (Cidades)obj;
+            if (ctrl != null)
+                aCtrl = (Controller)ctrl;
+        }
+        public override void Salvar()
+        {
+            // if ( MessageDlg("Confirma (S/N)") == "S" )
+            aCidade.Codigo = Convert.ToInt32(txtCodigo.Text);
+            aCidade.Cidade = txtCidade.Text;
+            aCidade.Ddd = txtDdd.Text;
+
+            //aCtrl.Salvar(oPais);
+        }
+        public override void CarregaTxt()
+        {
+            this.txtCodigo.Text = Convert.ToString(aCidade.Codigo);
+            this.txtCidade.Text = aCidade.Cidade;
+            this.txtDdd.Text = aCidade.Ddd;
+
+
+        }
+
+        public override void LimpaTxt()
+        {
+            this.txtCodigo.Text = "0";
+            this.txtCidade.Clear();
+            this.txtDdd.Clear();
+
+        }
+        public override void BloquearTxt()
+        {
+            this.txtCidade.Enabled = false;
+            this.txtDdd.Enabled = false;
+        }
+        public override void DesbloquearTxt()
+        {
+            this.txtCidade.Enabled = true;
+            this.txtDdd.Enabled = true;
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string btnSair = oFrmConsEstados.btnSair.Text;
+            oFrmConsEstados.btnSair.Text = "Selecionar";
+            oFrmConsEstados.ConhecaObj(aCidade.OEstado, aCtrl);
+            oFrmConsEstados.ShowDialog();
+            this.txtCodigoEstado.Text = Convert.ToString(aCidade.OEstado.Codigo);
+            this.txtEstado.Text = aCidade.OEstado.Estado.ToString();
+            oFrmConsEstados.btnSair.Text = btnSair;
+        }
+    }
+}
+
+
---
+++ ProjetoElp4Paises/FrmConsPaises.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetoElp4Paises
+{
+    public partial class FrmConsPaises : ProjetoElp4Paises.FrmConsultas
+    {
+        FrmCadPaises oFrmCadPaises;
+        Paises oPais;
+        Controller aCtrl;
+        public FrmConsPaises()
+        {
+            InitializeComponent();
+            oFrmCadPaises = new FrmCadPaises();
+        }
+        protected override void Pesquisar()
+        {
+
+        }
+        protected override void Incluir()
+        {
+            oFrmCadPaises.LimpaTxt();
+            oFrmCadPaises.ConhecaObj(oPais, aCtrl);
+            oFrmCadPaises.ShowDialog();
+        }
+        protected override void Alterar()
+        {
+            oFrmCadPaises.ConhecaObj(oPais, aCtrl);
+            oFrmCadPaises.LimpaTxt();
+            oFrmCadPaises.CarregaTxt();
+            oFrmCadPaises.ShowDialog();
+        }
+        protected override void Excluir()
+        {
+            string aux;
+            oFrmCadPaises.ConhecaObj(oPais, aCtrl);
+            oFrmCadPaises.LimpaTxt();
+            oFrmCadPaises.CarregaTxt();
+            oFrmCadPaises.BloquearTxt();
+            aux = oFrmCadPaises.btnSalvar.Text;
+            oFrmCadPaises.btnSalvar.Text = "Excluir";
+            oFrmCadPaises.ShowDialog();
+            oFrmCadPaises.DesbloquearTxt();
+            oFrmCadPaises.btnSalvar.Text = aux;
+        }
+        public override void setFrmCadastro(object obj)
+        {
+            if (obj != null) 
+                oFrmCadPaises = (FrmCadPaises) obj;
+        }
+        public override void ConhecaObj(object obj, object ctrl)
+        {
+            if(obj != null)
+                oPais = (Paises) obj;
+            if(ctrl != null)
+                aCtrl = (Controller) ctrl;
+        }
+    }
+}
